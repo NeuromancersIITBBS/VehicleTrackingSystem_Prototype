7cc2c1b02118b6ad82cf6a278447063eb3b32b27
rishabh gupta <gupta.rishabh606@gmail.com>
2019-10-28 00:04:57 +0530

    last changes

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -1,5 +1,17 @@
-let vehicleData = require('../models/vehicleData.js');
+let vehicleData = require('../models/vehicleData.js').vehicleData;
 
-async function getLocation(){
-
-}
+let getLocation = async function getLocation(vehicleId,map) {
+    let vehicleList = await vehicleData();
+    let longitude = vehicleList[vehicleId].longitude;
+    let latitude = vehicleList[vehicleId].latitude;
+   // let seats = vehicleList[vehicleId].seats;
+    let options = {
+        position: {
+            lat: latitude,
+            lng: longitude,
+        },
+        map: map.gMap,
+    };
+    return options;
+};
+module.export = getLocation;
--- views/mapView.js
+++ views/mapView.js
@@ -1,10 +1,22 @@
-function initMap() {
+const vehicleLocation = require('../controllers/locationController').getLocation;
+
+
+async function initMap(vehicleId) {
     const options = {
         center: {
-            lat: "",
-            lng: "",
+            lat: 20.272610,
+            lng: 85.833120,
         },
         zoom: 11
     };
     let map = new google.maps.Map(document.getElementById('map'),options);
+    map.init(function () {
+        setInterval(function () {
+            let markerOptions = vehicleLocation(vehicleId,map);
+            let marker  = new google.maps.Marker(markerOptions)
+        },10000)
+    });
+
+
 }
+initMap(1);
